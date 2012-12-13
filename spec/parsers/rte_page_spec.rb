6dcf77ceaf3626c18722f564853c4ba92409029c
Iain McNulty <iainmcnulty@gmail.com>
2012-12-13 15:33:46 +0000

    RTE pages now use HTML5. Parser updated to parse pages correctly. Updated test fixture added to spec and rte_parser updated too

--- spec/parsers/rte_page_spec.rb
+++ spec/parsers/rte_page_spec.rb
@@ -36,7 +36,7 @@ describe RtePageParserV1 do
       @valid_options = { 
         :url => 'http://www.rte.ie/news/2012/0718/aer-lingus-repeats-call-to-reject-ryanair-offer-business.html',
         :page => File.read("spec/fixtures/rte/aer-lingus-repeats-call-to-reject-ryanair-offer-business.html"),
-        :valid_hash => '803be530c7c3360d136b73e5531e9083'
+        :valid_hash => 'cdad460c9cc6d54fb54355da8a1f3a8a'
       }
       @pa = RtePageParserV1.new(@valid_options)
     end
@@ -46,7 +46,7 @@ describe RtePageParserV1 do
     end
     
     it "should parse the date in UTC" do
-      @pa.date.should == DateTime.parse("2012-07-18T10:28:38")
+      @pa.date.should == DateTime.parse("2012-07-18T14:27:45")
       @pa.date.zone.should == '+00:00'
     end
 
@@ -58,6 +58,68 @@ describe RtePageParserV1 do
       @pa.content[9].should == "Ryanair is seeking acceptance of the bid by 13 September."
       @pa.content.last.should == "An investigation is also being carried out into Ryanair's current holding by the UK Competition Commission."
       @pa.content.size.should == 11         
+      @pa.hash.should == @valid_options[:valid_hash]
+    end
+  end
+
+  describe "when parsing the drinking article" do
+    before do
+      @valid_options = { 
+        :url => 'http://www.rte.ie/news/2012/1213/drink-driving-figures.html',
+        :page => File.read("spec/fixtures/rte/drink-driving-figures.html"),
+        :valid_hash => '7ab04fedfa177059c5057790b11c23c6'
+      }
+      @pa = RtePageParserV1.new(@valid_options)
+    end
+
+    it "should parse the title" do
+      @pa.title.should == "Drink-driving arrests fall by 1,800 annually"
+    end
+    
+    it "should parse the date in UTC" do
+      @pa.date.should == DateTime.parse("2012-12-13T11:52:02")
+      @pa.date.zone.should == '+00:00'
+    end
+
+
+    it "should parse the content" do
+      @pa.content[1].should == "Drink-driving arrests by gardaí have fallen by just over 1,800 in the past year after reduced alcohol levels for drivers were introduced."
+
+      @pa.content[2].should == "Figures show the number of arrests for the offence have halved in the past five years."
+      @pa.content[9].should == "Irish, Latvian and Lithuanian nationals were among the main repeat offenders."
+      @pa.content.last.should == "Irish, Latvian and Lithuanian nationals were among the main repeat offenders."
+      @pa.content.size.should == 10         
+      @pa.hash.should == @valid_options[:valid_hash]
+    end
+  end
+
+  describe "when parsing the gallagher article" do
+    before do
+      @valid_options = { 
+        :url => 'http://www.rte.ie/news/2012/1213/gallagher-death.html',
+        :page => File.read("spec/fixtures/rte/gallagher-death.html"),
+        :valid_hash => 'af49aedf5940a1bd3b2de525ee160564'
+      }
+      @pa = RtePageParserV1.new(@valid_options)
+    end
+
+    it "should parse the title" do
+      @pa.title.should == "Sister of Erin Gallagher dies tragically"
+    end
+    
+    it "should parse the date in UTC" do
+      @pa.date.should == DateTime.parse("2012-12-13T17:02:19")
+      @pa.date.zone.should == '+00:00'
+    end
+
+
+    it "should parse the content" do
+      @pa.content[1].should == "The 15-year-old sister of Erin Gallagher, who died in October by suicide in Ballybofey, Co Donegal, was found dead last night."
+
+      @pa.content[2].should == "Shannon Gallagher is also believed to have died by suicide."
+      @pa.content[9].should == "\"Currently the HSE is liaising with the family in relation to this incident and all supports are being made available to provide assistance at this difficult time.\""
+      @pa.content.last.should == "Pieta Mid-WestPieta Mid-West, Ardaulin, Mungret, Co LimerickTel: 061-484444 | 061-484646"
+      @pa.content.size.should == 20
       @pa.hash.should == @valid_options[:valid_hash]
     end
   end
